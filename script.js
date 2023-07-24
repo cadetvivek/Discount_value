eff9769d07ad4f16d949e4c5b8d522d4c127a181
vivek <106858243+cadetvivek@users.noreply.github.com>
2023-07-24 13:55:05 +0530

    Update script.js
    comment some line

--- script.js
+++ script.js
@@ -7,9 +7,9 @@ function calculateDiscount() {
     const discountAmount = totalPrice * discountPercentage;
     const discountedPrice = Math.floor(totalPrice - discountAmount);
     const savedMoney = Math.floor(totalPrice - discountedPrice);
-
+// The .toFixed(2) is a method in JavaScript that is used to format a number to a specified number of decimal places.
     document.getElementById('discountedPrice').textContent = `रू${discountedPrice.toFixed(2)}`;
     document.getElementById('savedMoney').textContent = `रू${savedMoney.toFixed(2)}`;
-
+// document: This is the JavaScript document object, which represents the DOM (Document Object Model) of the current web page. It provides access to all elements and content on the page.
     document.getElementById('output').style.display = 'block';
 }
